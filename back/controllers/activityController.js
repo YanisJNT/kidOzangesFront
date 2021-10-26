40788fb076917503c5836aabd32030d41071a135
FabienNaveau <fabien.naveau@gmail.com>
2021-10-26 17:32:13 +0200

    Ajout de l'architecture back

---
+++ back/controllers/activityController.js
@@ -0,0 +1,9 @@
+const activityDataMapper = require("../datamappers/activityDataMapper");
+const userDataMapper = require("../datamappers/userDataMapper");
+const adminDataMapper = require("../datamappers/adminDataMapper");
+
+const activityController = {
+
+};
+
+module.exports = activityController;
