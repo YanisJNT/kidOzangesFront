599e83263ef8e2f3b250ba5faaca8f0e81f17c89
FabienNaveau <fabien.naveau@gmail.com>
2021-10-28 11:04:32 +0200

    Ajout du cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const router = require("./back/router.js")
+const router = require("./back/router.js");
+const cors = require("cors")
 
 const app = express();
 
@@ -11,6 +12,9 @@ const HOST = process.env.HOST
 app.use(express.static("front"))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors({
+    origin: "https://kidozanges.herokuapp.com"
+}));
 app.use(router);
 
 app.listen(PORT, HOST, () => {
