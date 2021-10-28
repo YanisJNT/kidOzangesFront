b3d9c062f6df160701e7967df414e7128689b8e5
FabienNaveau <fabien.naveau@gmail.com>
2021-10-28 12:21:42 +0200

    Essais heroku 8

--- back/controllers/mainController.js
+++ back/controllers/mainController.js
@@ -3,7 +3,7 @@ const path = require("path")
 const mainController = {
     getReactApp: (_,res) => {
         
-        res.sendFile((path.resolve(__dirname, "../../front/public", "index.html")))
+        res.sendFile((path.resolve(__dirname, "../../client/build", "index.html")))
     }
 
 }
