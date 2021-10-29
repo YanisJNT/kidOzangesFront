6e611df1372f1af9bca3a8ec634fdd29d3896d33
alex-eug <alexandre.eugene31@gmail.com>
2021-10-29 14:01:43 +0200

    envoi des données activité

--- front/src/components/SubmitActivity/index.js
+++ front/src/components/SubmitActivity/index.js
@@ -1,57 +1,72 @@
 import React, { Component, useState } from 'react';
+import axios from 'axios'
 import './style.css';
 import {
   Button,
-  Checkbox,
   Form,
   Input,
   Radio,
-  Select,
   TextArea,
 } from 'semantic-ui-react'
 
-
-const handleChange = () => {
-
-}
-
 export const SubmitActivity = () => {
+  const [titleActivity, setTitleActivity] = useState("");
+  const [descriptionActivity, setDescriptionActivity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  
 
-  const [state, setstate] = useState("")
+  const handleSubmitActivity = async (evt) => {
+    evt.preventDefault()
+    await axios.post("http://localhost:3000/api/submitactivity", {
+
+    })
+  }
+
   return (
 
     <div className="container">
-      <Form id="form--activity">
-        
-          <Form.Field
-            control={Input}
-            label="Titre de l'activitée"
-            placeholder="Titre de l'activitée"
-          />
-          <Form.Field
-            control={Input}
-            label='Last name'
-            placeholder='Last name'
-          />
-        
+      <Form id="form--activity" method="POST" onSubmit={handleSubmitActivity}>
+
+        <Form.Field
+          control={Input}
+          name='titleActivity'
+          label="Titre de l'activitée"
+          placeholder="Titre de l'activitée"
+          value={titleActivity}
+          onChange={(evt) => {setTitleActivity(evt.target.value)}}
+        />
+
+
         <Form.Field
           control={TextArea}
-          label='About'
-          placeholder='Tell us more about you...'
+          name='descriptionActivity'
+          label="Descrition de l'activitée"
+          placeholder="Descrition de l'activitée"
+          value={descriptionActivity}
+          onChange={(evt) => {setDescriptionActivity(evt.target.value)}}
         />
+        <Form.Field
+        name='postalCode'
+        label="Code Postale"
+        placeholder="Code Postale"
+        value={postalCode}
+        onChange={(evt) => {setPostalCode(evt.target.value)}}
+        />
+
+
         <Form.Group inline>
-          
+
           <Form.Field
             control={Radio}
             label='gratuite'
             value='gratuite'
-            onChange={handleChange}
+
           />
           <Form.Field
             control={Radio}
             label='payante'
             value='payante'
-            onChange={handleChange}
+
           />
         </Form.Group>
         <Form.Field id="form--activity__button" control={Button}>Submit</Form.Field>
