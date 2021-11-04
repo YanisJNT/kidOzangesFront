26748dc3d086be0a5790025d3a2e8a9f4cb39821
yayouu <sokay2000@outlook.fr>
2021-11-04 02:20:40 +0100

    add icon header

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 import { Rating } from 'semantic-ui-react'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
 
@@ -18,49 +19,55 @@ export default function Header() {
 
             <section className="box-best--actives">
 
-                <a href= "/detailactivity/67">
-                <article className="box-card">
-                    <div className="box--img--note">
-                        <img src="https://tse3.mm.bing.net/th?id=OIP.voR5IYjSALKRwo92e5gKPAHaEK&pid=Api&P=0&w=338&h=191" alt="" />
+                <div className="box-card" >
+                    <NavLink className="div-nav" to="/sqdqsdsqd">
+                        <article className="article--main">
+                            <div className="box--img--note">
+                                <img src="https://tse3.mm.bing.net/th?id=OIP.voR5IYjSALKRwo92e5gKPAHaEK&pid=Api&P=0&w=338&h=191" alt="" />
 
-                        <Rating className="star-rating" icon='star' defaultRating={3} maxRating={5} />
-                    </div>
-
-
-                    <div className="text">
-                        <h4>
-                            Titre de l'activité
-                        </h4>
-                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Facere libero a, consequatur cupiditate at dolores provident earum adipisci,
-                            iusto vitae unde laudantium nobis labore natus delectus nemo alias officiis explicabo.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ea nostrum,
-                            tempora eos exercitationem tenetur ullam labore officia eaque iste repellendus illum ipsa omnis, quos illo earum ipsum minima veritatis.</p>
-                    </div>
-
-                </article></a>
-
-                <article className="box-card">
-                    <div className="box--img--note">
-                        <img src="https://tse3.mm.bing.net/th?id=OIP.voR5IYjSALKRwo92e5gKPAHaEK&pid=Api&P=0&w=338&h=191" alt="" />
-
-                        <Rating className="star-rating" icon='star' defaultRating={3} maxRating={5} />
-                    </div>
+                                <Rating className="star-rating" icon='star' defaultRating={3} maxRating={5} />
+                            </div>
 
 
-                    <div className="text">
-                        <h4>
-                            Titre de l'activité
-                        </h4>
-                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Facere libero a, consequatur cupiditate at dolores provident earum adipisci,
-                            iusto vitae unde laudantium nobis labore natus delectus nemo alias officiis explicabo.
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ea nostrum,
-                            tempora eos exercitationem tenetur ullam labore officia eaque iste repellendus illum ipsa omnis, quos illo earum ipsum minima veritatis.</p>
-                    </div>
+                            <div className="text">
+                                <h4>
+                                    Titre de l'activité
+                                </h4>
+                                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Facere libero a, consequatur cupiditate at dolores provident earum adipisci,
+                                    iusto vitae unde laudantium nobis labore natus delectus nemo alias officiis explicabo.
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ea nostrum,
+                                    tempora eos exercitationem tenetur ullam labore officia eaque iste repellendus illum ipsa omnis, quos illo earum ipsum minima veritatis.</p>
+                            </div>
 
-                </article>
+                        </article>
+                    </NavLink>
+                </div>
 
+                <div className="box-card" >
+                    <NavLink className="div-nav" to="/sqdqsdsqd">
+                        <article className="article--main">
+                            <div className="box--img--note">
+                                <img src="https://tse3.mm.bing.net/th?id=OIP.voR5IYjSALKRwo92e5gKPAHaEK&pid=Api&P=0&w=338&h=191" alt="" />
+
+                                <Rating className="star-rating" icon='star' defaultRating={3} maxRating={5} />
+                            </div>
+
+
+                            <div className="text">
+                                <h4>
+                                    Titre de l'activité
+                                </h4>
+                                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                    Facere libero a, consequatur cupiditate at dolores provident earum adipisci,
+                                    iusto vitae unde laudantium nobis labore natus delectus nemo alias officiis explicabo.
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ea nostrum,
+                                    tempora eos exercitationem tenetur ullam labore officia eaque iste repellendus illum ipsa omnis, quos illo earum ipsum minima veritatis.</p>
+                            </div>
+
+                        </article>
+                    </NavLink>
+                </div>
             </section>
         </main>
     )
