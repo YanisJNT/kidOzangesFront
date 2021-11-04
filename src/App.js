f9e0fc31e55c72d6b865cb2c177ecd75a1c78839
yayouu <sokay2000@outlook.fr>
2021-11-04 09:23:45 +0100

    correction du p  sur la root

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         <Route path="/LegalNotice" exact>
         <LegalNotice />
         </Route>
-        <Route path="/Profil" exact>
+        <Route path="/profil" exact>
             <Profil />
         </Route>
         <Route>
