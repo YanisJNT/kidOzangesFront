26edc70b3097b1239324a47b68b1f4bba73a0e67
yayouu <sokay2000@outlook.fr>
2021-11-10 15:45:51 +0100

    Header responsive finis +  page detail activity finis

--- src/components/DetailActivity/Comments/index.js
+++ src/components/DetailActivity/Comments/index.js
@@ -5,31 +5,35 @@ import './style.css'
 
 //const date = new Date()
 
-const Comments = ({ listComment }) => (
-  <>
-    <Comment.Group
-      size='large'
-      id="chat"
-    >
-      {
-        listComment.map((com, index) => {
-          return (
-            <Comment key={index}  >
-              <Comment.Content>
-                <Comment.Author id="author" as='a'>{com.nickname}</Comment.Author>
+function Comments({ listComment }) {
+  console.log(listComment)
+  return (
+    <>
+      <Comment.Group
+        size='large'
+        id="chat"
+      >
+        {
+          listComment.map((com, index) => {
+            return (
+              <Comment key={index}  >
+                <div id="commentairebg">
+                  <Comment.Content>
+                    <Comment.Author id="author" as='a'>{com.nickname}</Comment.Author>
 
-                <Comment.Metadata>
-                  {/*<span>{date.getDate()}/{date.getMonth()}/{date.getFullYear()}</span>*/}
-                </Comment.Metadata>
-                <Comment.Text>{com.description}</Comment.Text>
-              </Comment.Content>
-            </Comment>
-          )
-        })
-      }
-    </Comment.Group>
-  </>
-)
-
+                    <Comment.Metadata>
+                      {/*<span>{date.getDate()}/{date.getMonth()}/{date.getFullYear()}</span>*/}
+                    </Comment.Metadata>
+                    <Comment.Text>{com.description}</Comment.Text>
+                  </Comment.Content>
+                </div>
+              </Comment>
+            )
+          })
+        }
+      </Comment.Group>
+    </>
+  )
+}
 
 export default Comments
