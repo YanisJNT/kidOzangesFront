a06cca6c53d273e3e22cb9b9dd2add3bc72790a9
alex-eug <alexandre.eugene31@gmail.com>
2021-11-15 16:31:00 +0100

    modal signalement ternaire note plus correction

--- src/components/DetailActivity/Comments/index.js
+++ src/components/DetailActivity/Comments/index.js
@@ -1,17 +1,21 @@
+
 import axios from 'axios'
-import React from 'react'
-import { Comment, Icon } from 'semantic-ui-react'
+import React, {useState} from 'react'
+import { Comment,  Button, Header, Icon, Modal } from 'semantic-ui-react'
 import './style.css'
 
 
 
 //const date = new Date()
 
-
 function Comments({ listComment }) {
   const token = localStorage.getItem("token")
+  const [open, setOpen] = useState(false)
   
+  
+
   const handleReportComment = async (commentId) => {
+    setOpen(true)
     try {
        await axios.patch(`https://kidozanges.herokuapp.com/api/user/reportcomment`, {report:'true'}, {
           params: {
@@ -26,11 +30,12 @@ function Comments({ listComment }) {
   } catch (error) {
       console.error(error)
   }
+  setTimeout(()=>setOpen(false),2000)
   }
-
 
   return (
     <>
+    
       <Comment.Group
         size='large'
         id="chat"
@@ -38,6 +43,7 @@ function Comments({ listComment }) {
         {
           listComment.map((com, index) => {
             return (
+              
               <Comment key={index}  >
                 <div id="commentairebg">
                   <Comment.Content>
@@ -45,17 +51,30 @@ function Comments({ listComment }) {
                     <Comment.Metadata>
                       {/*<span>{date.getDate()}/{date.getMonth()}/{date.getFullYear()}</span>*/}
                     </Comment.Metadata>
-                    <Comment.Text>{com.description} <Icon onClick={()=>handleReportComment(com.id)}  className="exclamation triangle" size="small" ></Icon></Comment.Text>
-                      
+                    <Comment.Text>{com.description} <Icon title="signaler ce commentaire" color="red" onClick={()=>handleReportComment(com.id)}  className="exclamation triangle" size="small" ></Icon></Comment.Text>
                   </Comment.Content>
                 </div>
+                
               </Comment>
             )
           })
         }
+        
       </Comment.Group>
+      <Modal
+      closeIcon
+      open={open}
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+    >
+      
+      <Modal.Content>
+        <p id="alert__modal">
+         Merci, ce commentaire a bien été signalé.
+        </p>
+      </Modal.Content>
+    </Modal>
     </>
   )
 }
-
 export default Comments
