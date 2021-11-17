6e1ae3e8262db42ff9642a7ededdb19caa1662e8
FabienNaveau <fabien.naveau@gmail.com>
2021-11-17 16:32:15 +0100

    Fermeture du menu burger au clic sur une nav

--- src/components/Header/slidebar/index.js
+++ src/components/Header/slidebar/index.js
@@ -21,21 +21,21 @@ export default function slideBar() {
             if(dataToken.role === "user") {
                 return (
                     <nav className="navBarTic">
-                        <NavLink to="/submitactivity">Soumettre une activité</NavLink>
-                        <NavLink to="/recherche">Rechercher une activité</NavLink>
-                        <NavLink to="/profil">Profil</NavLink>
-                        <NavLink to="/logout">Déconnexion</NavLink>
+                        <NavLink to="/submitactivity" onClick={closeHamburger}>Soumettre une activité</NavLink>
+                        <NavLink to="/recherche" onClick={closeHamburger}>Rechercher une activité</NavLink>
+                        <NavLink to="/profil" onClick={closeHamburger}>Profil</NavLink>
+                        <NavLink to="/logout" onClick={closeHamburger}>Déconnexion</NavLink>
                     </nav>
     
                 )
             } else if (dataToken.role === "admin") {
                 return (
                 <nav className="navBarTic">
-                        <NavLink to="/admin">Administrer</NavLink>
-                        <NavLink to="/submitactivity">Soumettre une activité</NavLink>
-                        <NavLink to="/recherche">Rechercher une activité</NavLink>
-                        <NavLink to="/profil">Profil</NavLink>
-                        <NavLink to="/logout">Déconnexion</NavLink>
+                        <NavLink to="/admin" onClick={closeHamburger}>Administrer</NavLink>
+                        <NavLink to="/submitactivity" onClick={closeHamburger}>Soumettre une activité</NavLink>
+                        <NavLink to="/recherche" onClick={closeHamburger}>Rechercher une activité</NavLink>
+                        <NavLink to="/profil" onClick={closeHamburger}>Profil</NavLink>
+                        <NavLink to="/logout" onClick={closeHamburger}>Déconnexion</NavLink>
                 </nav>
                 )
             }
@@ -43,9 +43,9 @@ export default function slideBar() {
         } else {
             return (
                 <nav className="navBarTic">
-                    <NavLink to="/recherche">Rechercher une activité</NavLink>
-                    <NavLink to="/signup">Inscription</NavLink>
-                    <Connexion />
+                    <NavLink to="/recherche"onClick={closeHamburger}>Rechercher une activité</NavLink>
+                    <NavLink to="/signup"onClick={closeHamburger}>Inscription</NavLink>
+                    <Connexion onClick={closeHamburger}/>
                 </nav>
             )
         }
